b1a991701d0c65c2745eb1187046b40f65b7b4b5
darkslave86 <destroyed-666@ya.ru>
2015-04-01 00:48:58 +0500

    netty impl

---
+++ src/main/java/net/darkslave/stm/server/netty/tcp/ServerFactoryImpl.java
@@ -0,0 +1,18 @@
+package net.darkslave.stm.server.netty.tcp;
+
+import java.io.IOException;
+import net.darkslave.stm.core.Server;
+import net.darkslave.stm.core.ServerConfig;
+import net.darkslave.stm.core.ServerFactory;
+
+
+
+
+public class ServerFactoryImpl implements ServerFactory {
+
+    @Override
+    public Server create(ServerConfig config) throws IOException {
+        return new ServerImpl(config);
+    }
+
+}
