e3a753c6da578144c02a4c9f0dc91e8a5afe9110
darkslave86 <destroyed-666@ya.ru>
2015-04-08 20:05:44 +0600

    refactoring

--- src/main/java/net/darkslave/stm/core/Message.java
+++ src/main/java/net/darkslave/stm/core/Message.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.channels.ByteChannel;
+import net.darkslave.io.Channels;
 import net.darkslave.io.Streams;
 
 
@@ -99,11 +100,36 @@ public class Message {
     }
 
 
-    public static Message readFrom(ByteChannel channel) {
+    public static Message readFrom(ByteChannel stream) throws IOException {
+        byte[] temp = new byte[6];
+        int read;
 
+        read = Channels.read(stream, temp);
+        if (read != 6) {
+            return null;
+        }
 
-        // TODO Auto-generated method stub
-        return null;
+        int mark = 0;
+        mark|= (temp[0] & 255) << 8;
+        mark|= (temp[1] & 255);
+
+        if (mark != MARKER)
+            throw new IOException("Invalid Message packet");
+
+        int size = 0;
+        size|= (temp[2] & 255) << 24;
+        size|= (temp[3] & 255) << 16;
+        size|= (temp[4] & 255) <<  8;
+        size|= (temp[5] & 255);
+
+        byte[] data = new byte[size];
+
+        read = Channels.read(stream, data);
+        if (read != size) {
+            return null;
+        }
+
+        return new Message(data, size);
     }
 
 
